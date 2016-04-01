872c3de1dcdf488ef05f5138ec7afdc6653f5179
Gerard Downes <gdownes@gmail.com>
2016-04-01 01:41:09 +0100

    Added data source configuration

---
+++ src/main/java/com/codeaim/urlcheck/probe/configuration/DataSourceConfiguration.java
@@ -0,0 +1,34 @@
+package com.codeaim.urlcheck.probe.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.dataSourceClassName}")
+    private String dataSourceClassName;
+
+    @Bean
+    public DataSource primaryDataSource()
+    {
+        Properties dataSourceProperties = new Properties();
+        dataSourceProperties.put("url", dataSourceUrl);
+
+        Properties hikariProperties = new Properties();
+        hikariProperties.put("dataSourceClassName", dataSourceClassName);
+        hikariProperties.put("dataSourceProperties", dataSourceProperties);
+
+        HikariConfig hikariConfig = new HikariConfig(hikariProperties);
+        return new HikariDataSource(hikariConfig);
+    }
+}
